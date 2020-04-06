338e42299cd11e2a6ae92f7d0fd87b28107fe1af
Willian Serafim <willian_50@ymail.com>
2020-04-05 21:35:32 -0300

    Calculadora

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Calculadora from './main/Calculadora'
+// import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <h1>Calculadora</h1>
+      <Calculadora />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
